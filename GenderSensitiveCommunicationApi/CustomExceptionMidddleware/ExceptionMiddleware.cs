48aac14196288f0235f81b2d7aa76d3720815227
Yagmur Solmaz <52479605+yaagmurss@users.noreply.github.com>
2022-12-11 16:38:54 +0300

    inital commit

---
+++ GenderSensitiveCommunicationApi/CustomExceptionMidddleware/ExceptionMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using GenderSensitiveCommunicationApi.ErrorHandling;
+
+namespace GenderSensitiveCommunicationApi.CustomExceptionMidddleware
+{
+   
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        //if anything goes wrong in http pipeline, It will be catched in below 
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (LanguageViolationException lanEx)
+            {             
+                await HandleExceptionAsync(httpContext, lanEx);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync("Gender Sensitive Communication Rules Broken...");
+        }
+    }
+    
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ GenderSensitiveCommunicationApi/ErrorHandling/LanguageViolationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GenderSensitiveCommunicationApi.ErrorHandling
+{
+    [Serializable]
+    public class LanguageViolationException : Exception
+    {
+        public LanguageViolationException(string message) : base(message) { }
+
+    }
+}
